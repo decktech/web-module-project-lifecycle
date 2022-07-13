16f6a184c65cf63fc72b44fb4df50f6c022ca75e
Matt Decker <matt@Matts-MBP.lan>
2022-07-13 18:10:12 -0400

    in progress

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -9,6 +9,7 @@ export default class App extends React.Component {
     error: '',
     nameInput: '',
   }
+
   onTodoNameInputChange = e => {
     const { value } = e.target 
     this.setState({
@@ -16,26 +17,35 @@ export default class App extends React.Component {
       nameInput: value
     })
   }
+
+  resetFormInput = () => {
+    this.setState({
+      ...this.state,
+      nameInput: ''
+    })
+  }
+
+  setAxiosResponseError = err => {
+    this.setState({
+      ...this.state,
+      error: err.response.data.message
+    })
+  }
+
   postNewTodo = () => {
     axios.post(URL, { name: this.state.nameInput })
       .then(res => {
-        this.fetchAllTodos()
-        this.setState({
-          ...this.state,
-          nameInput: ''
-        })
+        this.setState({ ...this.state, todos: this.state.todos.concact(res.data.data)})
+        this.resetFormInput()
       })
-      .catch(err => {
-        this.setState({
-          ...this.state,
-          error: err.response.data.message
-        })
-      })
+      .catch(this.setAxiosResponseError)
   }
+
   onTodoFormSubmit = e => {
     e.preventDefault()
     this.postNewTodo()
   }
+
   fetchAllTodos = () => {
     axios.get(URL)
       .then(res => {
@@ -44,16 +54,28 @@ export default class App extends React.Component {
           todos: res.data.data
         })
       })
-      .catch(err => {
+      .catch(this.setAxiosResponseError)
+      
+  }
+
+  toggleCompleted = id => () => {
+    axios.patch(`${URL}/${id}`)
+      .then(res => {
         this.setState({
           ...this.state,
-          error: err.response.data.message
+          todos: this.state.todos.map(todo => {
+            if(todo.id !== id) return todo
+            return res.data.data
+          })
         })
       })
+      .catch(this.setAxiosResponseError)
   }
+
   componentDidMount() {
     this.fetchAllTodos()
   }
+
   render() {
     return (
       <div>
@@ -62,7 +84,7 @@ export default class App extends React.Component {
           <h2>Todos:</h2>
           {
             this.state.todos.map(todo => {
-              return <div key={todo.id}>{todo.name}</div>
+              return <div onClick={this.toggleCompleted(todo.id)}key={todo.id}>{todo.name} {todo.completed ? ' ✔️' : ''}</div>
             })
           }
         </div>
